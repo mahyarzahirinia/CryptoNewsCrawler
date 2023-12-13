59fc7506be4e0941f1c75c0e3254d5aa8669cadb
mahyar <zahiriniamahyar@gmail.com>
2023-12-13 19:20:00 +0330

    init commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    print('start')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
