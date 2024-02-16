ad9876add38c6e128c332bd4942b6720c0d75c7c
mahyar <zahiriniamahyar@gmail.com>
2024-02-16 15:51:51 +0330

    fix: Telegram Bot converted to class and its working

--- main.py
+++ main.py
@@ -1,57 +1,34 @@
-# Crypto News Crawler
-# this app is designed to crawl websites and extract
-# crypto related information and rate them
 import asyncio
-
-from parser import Parser
+from telegram import Bot
+from colorama import Fore, Style
 
 from telegram_bot import NovncyBot
 
 
-# from chatgpt_translation import initiate as initiate_gpt, translate
-
-
-def main():
-    # @cryptocrawler_novncy_bot
-    telegram_token = '6852678484:AAGMa2Vt5yma_L2-NztGB_0XC7v1jGzeHW0'
-    channel_id = '@cryptocrawler_novncy_bot'
-    chatgpt_token = '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'
-    chatgpt_token2 = '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'
+async def main():
+    bot_name = '@cryptocrawler_novncy_bot'
+    bot_token = '6852678484:AAGMa2Vt5yma_L2-NztGB_0XC7v1jGzeHW0'
+    channel_name = '@test_novncy_bot_api'
     channel_id = '2052460067'
     url = 'https://coinmarketcap.com/headlines/news/'
+    chatgpt_token = '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'
+    chatgpt_token2 = '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'
 
-    # try:
-    #     init_bot(token=telegram_token)
-    #     send_message(channel_id, 'testing')
-    #
-    # except Exception as e:
-    #     print('exception happened on send_message')
+    try:
+        bot = NovncyBot(token=bot_token)
+        await bot.send_message(channel_name=channel_name, message="testing again2")
+    except Exception as e:
+        print(f"{Fore.RED}Error sending message: {e}{Style.RESET_ALL}")
 
     # Fetch and parse the website
     try:
-        parser = Parser(url)
-        news = parser.coinmarket_cap_stripper()
-        print('success')
+        # Uncomment this section once Parser class is defined
+        # parser = Parser(url)
+        # news = parser.coinmarket_cap_stripper()
+        print('Website parsing and news extraction success')
     except RuntimeError as e:
-        print(e)
-
-    # asyncio.run(run_telegram_bot(token=telegram_token, channel_id=channel_id))
-
-    # try:
-    #     initiate(telegram_token)
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-
-    # try:
-    #     initiate_gpt(chatgpt_token2)
-    #     translated = translate("I'm here to test something", 'persian')
-    #     print(translated)
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
+        print(f"{Fore.RED}Error parsing website: {e}{Style.RESET_ALL}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,19 +1,10 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import telegram
+from telegram import Bot
 
 
 class NovncyBot:
-    def __init__(self, token):
-        """Start the bot."""
-        # Create the Application and pass it your bot's token.
-        application = Application.builder().token("TOKEN").build()
+    def __init__(self, token: str):
+        self.bot = Bot(token=token)
 
-        # on different commands - answer in Telegram
-        # application.add_handler(CommandHandler("start", start))
-        # application.add_handler(CommandHandler("help", help_command))
-
-        # on non command i.e message - echo the message on Telegram
-        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-        # Run the bot until the user presses Ctrl-C
-        application.run_polling(allowed_updates=Update.ALL_TYPES)
+    async def send_message(self, channel_name: str, message: str):
+        await self.bot.send_message(chat_id=channel_name, text=message)
