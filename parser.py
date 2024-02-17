c4f16ca4e0e8f0b0807a5f00f905a8a464d03bed
mahyar <zahiriniamahyar@gmail.com>
2024-02-17 16:47:31 +0330

    draft: saving work

--- parser.py
+++ parser.py
@@ -1,3 +1,8 @@
+import asyncio
+
+from deepdiff import DeepDiff
+import array
+
 import requests
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
@@ -6,9 +11,13 @@ from colorama import Fore, Style
 
 class Parser:
     def __init__(self, url):
+        self._url = url
+        self._soup = None
+
+    def initialize_soup(self):
         try:
             # Send a GET request to the URL
-            response = requests.get(url)
+            response = requests.get(self._url)
 
             # Check if the request was successful (status code 200)
             response.raise_for_status()
@@ -27,10 +36,14 @@ class Parser:
             elements = self._soup.find('div', id=element_name)
         return elements
 
-    def coinmarket_cap_stripper(self):
+    def coinmarket_cap_stripper(self, soup_instance):
         # all the news items have parent div with class of uikit-row
+        if soup_instance is None:
+            raise TypeError(f"{Fore.RED}Error: soup instance is None{Style.RESET_ALL}")
+
+        soup_instance = self._soup
         all_news = []
-        all_sections = self._soup.find_all('div', class_="uikit-row")
+        all_sections = soup_instance.find_all('div', class_="uikit-row")
         if isinstance(all_sections, list):
             all_sections.pop(0)
         else:
@@ -74,3 +87,29 @@ class Parser:
                 # Handle the exception (e.g., print a message, log, or ignore)
                 print(f"{Fore.RED}error processing news sections {index}: {e}{Style.RESET_ALL}")
         return all_news
+
+    def updater(self, current_news: list, previous_news: list) -> dict:
+        isupdate = False
+        final = None
+        if not isinstance(current_news, list) or not isinstance(previous_news, list):
+            raise TypeError(f"{Fore.RED}either current or previous arguments is not list{Style.RESET_ALL}")
+
+        if current_news == previous_news:
+            final = current_news
+        else:
+            final = DeepDiff(current_news, previous_news)
+            isupdate = True
+            # here make sure to construct the list desired
+
+        # format both side of conditional to be the same for return
+        return {'isupdate': isupdate, 'diff': final}
+
+    async def timer(self, duration, func, *args, **kwargs):
+        await asyncio.sleep(duration)
+        await func(*args, **kwargs)
+
+    async def get_update(self, time_in_seconds: int):
+        if time_in_seconds <= 0:
+            return None
+
+        await self.timer(time_in_seconds, self.updater, )
