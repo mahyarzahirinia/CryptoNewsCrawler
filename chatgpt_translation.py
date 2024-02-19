8d2bd3f0cf6bc37fccd5d48a3d8bcc01ca73bb66
mmd <zahiriniamahyar@gmail.com>
2024-02-19 20:17:29 +0330

    fix: api key fixed

--- chatgpt_translation.py
+++ chatgpt_translation.py
@@ -1,20 +1,26 @@
 from openai import OpenAI
 from config import chatgpt_token
 
+from colorama import Fore, Style
+
 
 class ChatGPTTranslator:
     def __init__(self):
         self.client = OpenAI(api_key=chatgpt_token)
 
     def translate(self, text, target_language="Persian", except_following="urls"):
-        response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            response_format={"type": "json_object"},
-            messages=[
-                {"role": "system",
-                 "content": f"Translate the following text to {target_language} except the {except_following} "
-                            f"and maintain the formatted string intact please."},
-                {"role": "user", "content": text}
-            ]
-        )
-        print(response.choices[0].message.content)
+        try:
+            response = self.client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                response_format={"type": "json_object"},
+                messages=[
+                    {"role": "system",
+                     "content": f"Translate the following text to {target_language} except the {except_following} "
+                                f"and maintain the formatted string intact please."},
+                    {"role": "user", "content": text}
+                ]
+            )
+            print("test")
+            return response.choices[0].message.content
+        except Exception as e:
+            raise Exception(f"{Fore.RED}{e}{Style.RESET_ALL}")
