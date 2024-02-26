15c29b79f2927a848cff76d08923673b278f4c08
mmd <zahiriniamahyar@gmail.com>
2024-02-27 01:11:48 +0330

    fix: adding driver path and requirements

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ chatgpt_token = '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'
 chatgpt_token2 = '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'
 gemini_token = 'AIzaSyB3aR4D_GR4t5EjSs2TrQGT9ZIOGDA_opc'
 
-chrome_driver_path = '/bot_driver/chromedriver'
+chrome_driver_path = './bot_driver/chromedriver'
